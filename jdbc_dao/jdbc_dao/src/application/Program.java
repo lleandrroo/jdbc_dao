a91ba4c21e23377163dd796c8ae2eebc954a0616
lleandrroo <lleandrroo@hotmail.com>
2024-01-19 11:22:03 -0300

    seller

--- jdbc_dao/jdbc_dao/src/application/Program.java
+++ jdbc_dao/jdbc_dao/src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Connection conexao = DB.getConnection();
+		Department dp = new Department(1, "Books");
+		Seller sell = new Seller(11, "Bob", "bob@email.com", new Date(), 3000.0, dp);
+		System.out.println(sell);
 
 	}
 
